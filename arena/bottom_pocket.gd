4f923db1526aad6d6738cc96810fd06f0efbb311
Sion Arrowsmith <sion.arrowsmith@gmail.com>
2020-08-28 08:11:05 +0100

    Discourage bouncing out of pocket

--- arena/bottom_pocket.gd
+++ arena/bottom_pocket.gd
@@ -9,13 +9,13 @@ onready var label = $Label
 var score
 
 
-
 func _ready():
 	score = ( 5 + (randi() % 20 )) * (4 - abs(identifier - 4))
-	label.text = "%d" % score
+	label.text = str(score)
 
 
 func _on_Pocket_body_entered(body):
 	emit_signal("scored", identifier, score)
 	if body is RigidBody2D:
 		body.can_sleep = true
+		body.gravity_scale *= 2
